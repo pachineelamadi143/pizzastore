e2ef1dd35862cb1a94e9064df674a47ba0351eab
Aditya <aditya@gmail.com>
2026-03-16 11:39:00 +0530

    Show profile photo in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -173,10 +173,19 @@ function renderLinks(user, unreadCount, adminUnreadCount, resetAdminUnread, go, 
             onClick={() => setUserMenuOpen(!userMenuOpen)}
             style={{ textDecoration: 'none' }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-              <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-            </svg>
+            {user.profileImage ? (
+              <img
+                src={user.profileImage}
+                alt={user.name || 'Profile'}
+                className="rounded-circle border border-white"
+                style={{ width: '32px', height: '32px', objectFit: 'cover' }}
+              />
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+              </svg>
+            )}
             {!mobile && <i className={`bi bi-chevron-${userMenuOpen ? 'up' : 'down'} ms-1 small`}></i>}
           </button>
 
